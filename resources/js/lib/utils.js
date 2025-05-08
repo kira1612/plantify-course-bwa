e436cb540e33e259858f01be53ccf1daf9b54482
kira1612 <vikiramadhan16@gmail.com>
2025-05-08 15:14:35 +0700

    flash message

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -1,20 +1,23 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
-export const STATUS ={
-  TODO: 'To Do',
-  INPROGRESS: 'In Progress',
-  ONREVIEW: 'On Review',
-  DONE: 'Done',
-  UNKNOWN: 'Unknown',
+export const STATUS = {
+    TODO: 'To Do',
+    INPROGRESS: 'In Progress',
+    ONREVIEW: 'On Review',
+    DONE: 'Done',
+    UNKNOWN: 'Unknown',
 };
-export const PRIORITY ={
-  URGENT: 'Urgent',
-  HIGH: 'High',
-  MEDIUM: 'Medium',
-  LOW: 'Low',
-  UNKNOWN: 'Unknown',
+export const PRIORITY = {
+    URGENT: 'Urgent',
+    HIGH: 'High',
+    MEDIUM: 'Medium',
+    LOW: 'Low',
+    UNKNOWN: 'Unknown',
 };
+export function flashMessage(params) {
+    return params.props.flash_message;
+}
