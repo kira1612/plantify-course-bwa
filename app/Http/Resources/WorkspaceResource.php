33862080a42eed4423dbae03c41a5a9371364482
kira1612 <vikiramadhan16@gmail.com>
2025-05-21 16:11:06 +0700

    show workspace

--- app/Http/Resources/WorkspaceResource.php
+++ app/Http/Resources/WorkspaceResource.php
@@ -21,8 +21,7 @@ class WorkspaceResource extends JsonResource
             'slug' => $this->slug,
             'visibility' => $this->visibility?->value,
             // 'visibility' => $this->visibility ? $this->visibility->value : null,
-            'cover' => Storage::url(ltrim($this->cover, '/')),
-            // 'cover' => Storage::url($this->cover),
+            'cover' => Storage::url($this->cover),
             // 'cover' => $this->cover ? Storage::url($this->cover) : null,
             'logo' => Storage::url($this->logo),
         ];
